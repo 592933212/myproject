32a1d118703f47f108849c9e35a4446cdad7ae8e
lvyihang <592933212@qq.com>
2018-09-12 20:25:55 +0800

    change3

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Home from '@/components/children/home.vue'
 import Issues from '@/components/children/next/issues.vue'
 import Requests from '@/components/children/next/requests.vue'
 import Header from '@/components/children/header.vue'
-import Demo from '@/components/children/demo.vue'
 import Explore from '@/components/children/next/explore.vue'
 import MarketPlace from '@/components/children/next/marketplace.vue'
 
@@ -11,11 +10,6 @@ const routers = [
     path: '/',
     name: 'home',
     component: Home
-  },
-  {
-    path: '/demo',
-    name: 'demo',
-    component: Demo
   },
   {
     path: '/header',
